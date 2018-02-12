89a87e1283f7f0826424b04b6eef5092bd1548cf
Hritik Soni <hothritik1@gmail.com>
2018-02-12 22:50:01 +0530

    Removed Unused Stopword Reading

--- A12/docMaker.cpp
+++ A12/docMaker.cpp
@@ -16,16 +16,11 @@ int main(int argc, char * argv[]){
 	MPI_Init(&argc,&argv);
 
 	const int wlslen = 69903; //Known Wordlist Length
-	const int stlslen = 174; //Known Stopword List Length
 	
 	const int docwlen = 10000; //Desired Document Word Length
         const int docn = 10; //Desired Number of Documents
 
-	printf("Wordlist Length: %d Stopword List Length: %d\n",wlslen, stlslen);
-	//char* wlist[] = (char**)calloc(wlistlength, sizeof(char*));
-	
-	string wlist[wlslen];	
-	string stls[stlslen];
+	string wlist[wlslen];//Store WordList on RAM	
 
 	ifstream wfile("wordlist");
 
@@ -36,20 +31,13 @@ int main(int argc, char * argv[]){
 		++i;
 	}
 
-	ifstream sfile("stopwords");
-
-	i = 0;
-	while (sfile >> s){
-		stls[i] = s;	++i;
-	}
-
 	ofstream out;
 
 	for(int cur_doc=0; cur_doc < docn; ++cur_doc){
-		out.open("doc-"+to_string(cur_doc));
+		out.open("doc-"+to_string(cur_doc));//Create new document
 		for(int w_i=0; w_i<docwlen; ++w_i){
 			int w_index = rand() % wlslen;
-			out << wlist[w_index] << " ";
+			out << wlist[w_index] << " ";//Add random word to the document
 		}
 		out << endl;
 		out.close();
